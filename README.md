da51b0844689f2ad2077411310f2cf43fcd925e3
Zyta <zyta.slowianska@gmail.com>
2023-03-17 10:15:15 +0100

    feat: Element info parsed

--- README.md
+++ README.md
@@ -20,11 +20,3 @@ FROM elements e
   INNER JOIN types t
     ON p.type_id=t.type_id;
 ```
-
-## What is still left to do:
-- If you run ./element.sh 1, ./element.sh H, or ./element.sh Hydrogen, it should output only The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius.
-```sh
-"It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-```
-
-- If you run ./element.sh script with another element as input, you should get the same output but with information associated with the given element.
--- element.sh
+++ element.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+PARSE_INFO() {
+  echo $1 | while IFS="|" read ATOMIC_NUMBER SYMBOL NAME ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE
+  do
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  done
+}
+
 if [[ -z $1 ]]
 then
   echo -e "Please provide an element as an argument."
 else
-  if [[ $1 =~ ^[0-9]+$ ]] # is the argument a number?
+  if [[ $1 =~ ^[0-9]+$ ]]
   then
     ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e INNER JOIN properties p on e.atomic_number=p.atomic_number INNER JOIN types t ON p.type_id=t.type_id WHERE e.atomic_number=$1")
-  else # it's a string, so query using symbol and name db columns
+  else
     ELEMENT_INFO=$($PSQL "SELECT e.atomic_number, e.symbol, e.name, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e INNER JOIN properties p on e.atomic_number=p.atomic_number INNER JOIN types t ON p.type_id=t.type_id WHERE e.symbol='$1' OR e.name='$1';")
   fi
   if [[ -z $ELEMENT_INFO ]]
   then
     echo -e "I could not find that element in the database."
   else
-    echo $ELEMENT_INFO
+    PARSE_INFO $ELEMENT_INFO
   fi
 fi
